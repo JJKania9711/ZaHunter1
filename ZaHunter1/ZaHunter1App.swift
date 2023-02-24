5234bcabc4e5e928c50f274b13bd93ae150a0e03
JJKania9711 <116130334+JJKania9711@users.noreply.github.com>
2023-02-24 07:37:34 -0800

    Initial Commit

---
+++ ZaHunter1/ZaHunter1App.swift
@@ -0,0 +1,17 @@
+//
+//  ZaHunter1App.swift
+//  ZaHunter1
+//
+//  Created by James on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ZaHunter1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
